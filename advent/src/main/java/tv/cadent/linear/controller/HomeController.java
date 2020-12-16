05bd19362d0dec2dd566473012faa0d407cf362b
YamineeRajoria <YamineeRajoria@CSJ-YRAJORIA>
2020-12-16 12:22:20 -0800

    Dummy Json until index.html reached.

--- advent/src/main/java/tv/cadent/linear/controller/HomeController.java
+++ advent/src/main/java/tv/cadent/linear/controller/HomeController.java
@@ -1,5 +1,7 @@
 package tv.cadent.linear.controller;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -9,6 +11,8 @@ import java.util.stream.Stream;
 
 import javax.annotation.PostConstruct;
 
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
@@ -18,81 +22,61 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	
+
 	@Autowired
 	ResourceLoader resourceLoader;
 
 	private static Map<String, Float[]> geoLatLongs = new HashMap<String, Float[]>();
-	
+
 	@PostConstruct
 	public void loadLatLongs() {
 		Resource resource = resourceLoader.getResource("classpath:geodata/geodata.csv");
-        //read file into stream, try-with-resources
-        try (Stream<String> stream = Files.lines(Paths.get(resource.getURI()))) {
-            stream.forEach(line -> {
-            	String[] zeoLatLong = line.split(",");
-            	try {
-            		geoLatLongs.put(zeoLatLong[0], new Float[] {Float.valueOf(zeoLatLong[1]), Float.valueOf(zeoLatLong[2])});
-            	} catch (NumberFormatException nfe) {
-            		//Ignore
-            	}
-            });
-        } catch (IOException e) {
-           //Ignore
-        }
+		// read file into stream, try-with-resources
+		try (Stream<String> stream = Files.lines(Paths.get(resource.getURI()))) {
+			stream.forEach(line -> {
+				String[] zeoLatLong = line.split(",");
+				try {
+					geoLatLongs.put(zeoLatLong[0],
+							new Float[] { Float.valueOf(zeoLatLong[1]), Float.valueOf(zeoLatLong[2]) });
+				} catch (NumberFormatException nfe) {
+					// Ignore
+				}
+			});
+		} catch (IOException e) {
+			// Ignore
+		}
 	}
-	
-    @RequestMapping("/")
-    public String viewHome(Model model) {
-    	Object [][] latlongMappings = new Object[8][];
-    	latlongMappings[0] = new Object[] {
-    			"Latitude", 
-    			"Longitude",
-    			"Zip Code",
-    			};
-    	latlongMappings[1] = new Object[] {
-    			geoLatLongs.get("95014")[0], 
-    			geoLatLongs.get("95014")[1],
-    			"95014"
-    			};
-    	latlongMappings[2] = new Object[] {
-    			geoLatLongs.get("95017")[0], 
-    			geoLatLongs.get("95017")[1],
-    			"95017"
-    			};
-    	latlongMappings[3] = new Object[] {
-    			geoLatLongs.get("95018")[0], 
-    			geoLatLongs.get("95018")[1],
-    			"95018"
-    			};
-    	latlongMappings[4] = new Object[] {
-    			geoLatLongs.get("95019")[0], 
-    			geoLatLongs.get("95019")[1],
-    			"95019"
-    			};
-    	latlongMappings[5] = new Object[] {
-    			geoLatLongs.get("95020")[0], 
-    			geoLatLongs.get("95020")[1],
-    			"95020"
-    			};
-    	latlongMappings[6] = new Object[] {
-    			geoLatLongs.get("10001")[0], 
-    			geoLatLongs.get("10001")[1],
-    			"10001"
-    			};
-    	latlongMappings[7] = new Object[] {
-    			geoLatLongs.get("10005")[0], 
-    			geoLatLongs.get("10005")[1],
-    			"10005"
-    			};
-        model.addAttribute("latlongMappings", latlongMappings);
-        model.addAttribute("restrictedDevices", new String[] {"pc","tablet"});
-		model.addAttribute("feedData", new String[] { "pc", "tablet" });// Add
-																		// Maria's
-																		// Dummy
-																		// Json
-																		// here
-        return "index";
-    }
-}
 
+	@RequestMapping("/")
+	public String viewHome(Model model) {
+		Object[][] latlongMappings = new Object[8][];
+		latlongMappings[0] = new Object[] { "Latitude", "Longitude", "Zip Code", };
+		latlongMappings[1] = new Object[] { geoLatLongs.get("95014")[0], geoLatLongs.get("95014")[1], "95014" };
+		latlongMappings[2] = new Object[] { geoLatLongs.get("95017")[0], geoLatLongs.get("95017")[1], "95017" };
+		latlongMappings[3] = new Object[] { geoLatLongs.get("95018")[0], geoLatLongs.get("95018")[1], "95018" };
+		latlongMappings[4] = new Object[] { geoLatLongs.get("95019")[0], geoLatLongs.get("95019")[1], "95019" };
+		latlongMappings[5] = new Object[] { geoLatLongs.get("95020")[0], geoLatLongs.get("95020")[1], "95020" };
+		latlongMappings[6] = new Object[] { geoLatLongs.get("10001")[0], geoLatLongs.get("10001")[1], "10001" };
+		latlongMappings[7] = new Object[] { geoLatLongs.get("10005")[0], geoLatLongs.get("10005")[1], "10005" };
+		model.addAttribute("latlongMappings", latlongMappings);
+		model.addAttribute("restrictedDevices", new String[] { "pc", "tablet" });
+
+		JSONParser parser = new JSONParser();
+		try {
+			Resource resource = resourceLoader.getResource("classpath:feeddata/feedData.json");
+
+			File jsonFeedFile = new File(resource.getURI());
+			Object obj = parser.parse(new FileReader(jsonFeedFile));
+
+			// A JSON object. Key value pairs are unordered. JSONObject supports
+			// java.util.Map interface.
+			JSONArray feedJsonArray = (JSONArray) obj;
+
+			model.addAttribute("feedJsonArray", feedJsonArray);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return "index";
+	}
+}
